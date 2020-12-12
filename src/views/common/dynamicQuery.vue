beda156d9b59fda5681787556701fe2c55bb77c1
zhongyc <yanczhong@outlook.com>
2020-12-12 19:37:24 +0800

    新增table时间格式化配置

--- src/views/common/dynamicQuery.vue
+++ src/views/common/dynamicQuery.vue
@@ -19,13 +19,25 @@
             {{ statusColumn }}
           </a-tag>
         </template>
+        <template slot="dateFormat1" slot-scope="dateFormat1">
+          {{ getDate(dateFormat1, 'YYYY-MM-DD') }}
+        </template>
+        <template slot="dateFormat2" slot-scope="dateFormat2">
+          {{ getDate(dateFormat2, 'YYYY-MM-DD HH') }}
+        </template>
+        <template slot="dateFormat3" slot-scope="dateFormat3">
+          {{ getDate(dateFormat3, 'YYYY-MM-DD HH:mm') }}
+        </template>
+        <template slot="dateFormat4" slot-scope="dateFormat4">
+          {{ getDate(dateFormat4, 'YYYY-MM-DD HH:mm:ss') }}
+        </template>
       </a-table>
     </page-header-wrapper>
   </div>
 </template>
 <script>
 import { dynamic_button, dynamic_data } from '@/api/dynamic'
-
+import moment from 'moment'
 export default {
   data() {
     return {
@@ -34,20 +46,27 @@ export default {
       tableButtons: [],
       tableColumns: [],
       tableDatas: [],
-      statusMap: []
+      statusMap: [],
+      loading: false,
     }
   },
   mounted() {
-    dynamic_button(this.$route.meta.permission).then(response => {
+    dynamic_button(this.$route.meta.permission).then((response) => {
       this.conditionButtons = response.data.conditionButtons
       this.tableButtons = response.data.tableButtons
     })
     const vo = { menuId: this.$route.meta.permission, limit: 100, page: 1 }
-    dynamic_data(vo).then(response => {
-      this.tableColumns = response.data.column
-      this.tableDatas = response.data.list
-      this.statusMap = response.data.statusMap
-    })
+    this.loading = true
+    dynamic_data(vo)
+      .then((response) => {
+        this.tableColumns = response.data.column
+        this.tableDatas = response.data.list
+        this.statusMap = response.data.statusMap
+        this.loading = false
+      })
+      .catch(() => {
+        this.loading = false
+      })
   },
   filters: {
     statusFilter(type, app0) {
@@ -60,7 +79,7 @@ export default {
       } else {
         return defaultColor
       }
-    }
+    },
   },
   methods: {
     list() {
@@ -74,10 +93,21 @@ export default {
         this.$parent.$options.methods[actionName].bind(this.$parent)()
       }
     },
+    getDate(row, format) {
+      if (row == null || row == '') {
+        return null
+      } else {
+        if (format == null || format == '') {
+          return moment(row).format('YYYY-MM-DD')
+        } else {
+          return moment(row).format(format)
+        }
+      }
+    },
     rowClassName(record, index) {
       return index % 2 === 1 ? 'light-row' : ''
-    }
-  }
+    },
+  },
 }
 </script>
 <style lang="scss" scoped>
