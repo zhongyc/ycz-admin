59a2f8d7e51b94902fe548cd06d6af95555ef0f9
zhongyc <yanczhong@outlook.com>
2021-01-08 20:56:41 +0800

    动态增删改，新增autoComplete渲染

--- src/views/common/dynamicCrud.vue
+++ src/views/common/dynamicCrud.vue
@@ -49,6 +49,37 @@
                     item.required ? [item.name, { rules: [{ required: true, message: item.name + '不能为空' }] }] : []
                   "
                 />
+                <a-select
+                  show-search
+                  v-if="item.inputType == 4"
+                  style="width: 100%"
+                  :placeholder="'请选择' + item.name"
+                  :default-active-first-option="false"
+                  :show-arrow="false"
+                  :filter-option="false"
+                  :not-found-content="null"
+                  @search="handleAutoCompleteSearch"
+                  @focus="handleAutoCompleteFocus(item)"
+                  optionLabelProp="label"
+                  :allowClear="item.allowClear && item.allowClear == 1"
+                  v-decorator="
+                    item.required ? [item.name, { rules: [{ required: true, message: item.name + '不能为空' }] }] : []
+                  "
+                >
+                  <template v-for="(col, index_col) in autoCompleteItem.autoCompleteOptions">
+                    <a-select-option :value="col.obj['id']" :label="col.obj['name']" :key="index_col">
+                      <a-row>
+                        <template v-for="(displayColumn, index_display) in col.columns">
+                          <a-col :key="index_display" :span="displayColumn.span">
+                            <div>
+                              {{ col.obj[displayColumn.dataIndex] }}
+                            </div>
+                          </a-col>
+                        </template>
+                      </a-row>
+                    </a-select-option>
+                  </template>
+                </a-select>
               </a-form-item>
             </a-col>
           </template>
@@ -59,13 +90,15 @@
 </template>
 <script>
 import { dynamic_crud_column, dynamic_crud_relation_item } from '@/api/dynamicCrud'
+import { auto_complete } from '@/api/autoComplete'
 export default {
   data() {
     return {
       form: this.$form.createForm(this, { name: 'dynamic_crud_rule' }),
       crudId: null,
       columns: [],
-      type: null
+      type: null,
+      autoCompleteItem: {}
     }
   },
   mounted() {},
@@ -99,6 +132,16 @@ export default {
     },
     handleCancel(e) {
       this.type = ''
+    },
+    handleAutoCompleteFocus(item) {
+      this.autoCompleteItem = item
+    },
+    handleAutoCompleteSearch(value) {
+      var allRecords =
+        this.autoCompleteItem.autoCompleteAllRecords == null ? 0 : this.autoCompleteItem.autoCompleteAllRecords
+      auto_complete(value, this.autoCompleteItem.autoCompleteName, allRecords).then(response => {
+        this.autoCompleteItem.autoCompleteOptions = response.data
+      })
     },
     getTitle(type) {
       return type == 1 ? '新增' : type == 2 ? '修改' : type == 3 ? '查看明细' : ''
