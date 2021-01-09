b82825705ad63e795810317920d6c5aedb12d17c
zhongyc <yanczhong@outlook.com>
2021-01-09 12:55:37 +0800

    动态增删改新增Radio和inputArea组件渲染

--- src/views/common/dynamicCrud.vue
+++ src/views/common/dynamicCrud.vue
@@ -8,32 +8,26 @@
       @cancel="handleCancel"
       :maskClosable="1 > 2"
     >
-      <a-form :form="form" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+      <a-form :form="form">
         <a-row>
           <template v-for="(item, index) in columns">
-            <a-col :key="index" :span="8">
-              <a-form-item :label="item.name">
+            <a-col :key="index" :span="8" v-if="item.inputType != 6">
+              <a-form-item :label="item.name" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
                 <a-input
-                  :id="item.dataIndex"
                   v-if="item.inputType == 1"
                   :placeholder="'请输入' + item.name"
-                  v-decorator="
-                    item.required ? [item.name, { rules: [{ required: true, message: item.name + '不能为空' }] }] : []
-                  "
+                  v-decorator="[item.name, { rules: [{ required: item.required, message: item.name + '不能为空' }] }]"
                   :allowClear="item.allowClear && item.allowClear == 1"
                   :disabled="type == 1 ? item.addEditable != 1 : type == 2 ? item.updateEditable != 1 : false"
                 />
                 <a-select
-                  :ref="item.dataIndex"
                   v-if="item.inputType == 2"
                   show-search
                   :placeholder="'请选择' + item.name"
                   option-filter-prop="children"
                   :allowClear="item.allowClear && item.allowClear == 1"
                   :disabled="type == 1 ? item.addEditable != 1 : type == 2 ? item.updateEditable != 1 : false"
-                  v-decorator="
-                    item.required ? [item.name, { rules: [{ required: true, message: item.name + '不能为空' }] }] : []
-                  "
+                  v-decorator="[item.name, { rules: [{ required: item.required, message: item.name + '不能为空' }] }]"
                 >
                   <template v-for="(aitem, aindex) in item.relationItems">
                     <a-select-option :value="aitem.dataValue" :key="aindex">
@@ -42,18 +36,14 @@
                   </template>
                 </a-select>
                 <a-date-picker
-                  :ref="item.dataIndex"
                   v-if="item.inputType == 3"
                   :placeholder="'请选择' + item.name"
                   style="width:100%"
                   :allowClear="item.allowClear && item.allowClear == 1"
                   :disabled="type == 1 ? item.addEditable != 1 : type == 2 ? item.updateEditable != 1 : false"
-                  v-decorator="
-                    item.required ? [item.name, { rules: [{ required: true, message: item.name + '不能为空' }] }] : []
-                  "
+                  v-decorator="[item.name, { rules: [{ required: item.required, message: item.name + '不能为空' }] }]"
                 />
                 <a-select
-                  :ref="item.dataIndex"
                   show-search
                   v-if="item.inputType == 4"
                   style="width: 100%"
@@ -65,9 +55,7 @@
                   @focus="handleAutoCompleteFocus(item)"
                   optionLabelProp="label"
                   :allowClear="item.allowClear && item.allowClear == 1"
-                  v-decorator="
-                    item.required ? [item.name, { rules: [{ required: true, message: item.name + '不能为空' }] }] : []
-                  "
+                  v-decorator="[item.name, { rules: [{ required: item.required, message: item.name + '不能为空' }] }]"
                 >
                   <template v-for="(col, index_col) in autoCompleteItem.autoCompleteOptions">
                     <a-select-option :value="col.obj['id']" :label="col.obj['name']" :key="index_col">
@@ -83,6 +71,32 @@
                     </a-select-option>
                   </template>
                 </a-select>
+                <a-radio-group
+                  v-if="item.inputType == 5"
+                  :allowClear="item.allowClear && item.allowClear == 1"
+                  v-decorator="[item.name, { rules: [{ required: item.required, message: item.name + '不能为空' }] }]"
+                >
+                  <template v-for="(aitem, aindex) in item.relationItems">
+                    <a-radio :value="aitem.dataValue" :key="aindex">
+                      {{ aitem.name }}
+                    </a-radio>
+                  </template>
+                </a-radio-group>
+              </a-form-item>
+            </a-col>
+          </template>
+        </a-row>
+        <a-row>
+          <template v-for="(item, index) in columns">
+            <a-col :key="index" :span="24" v-if="item.inputType == 6">
+              <a-form-item :label="item.name" :label-col="{ span: 2 }" :wrapper-col="{ span: 22 }">
+                <a-textarea
+                  v-if="item.inputType == 6"
+                  :placeholder="'请输入' + item.name"
+                  v-decorator="[item.name, { rules: [{ required: item.required, message: item.name + '不能为空' }] }]"
+                  :allowClear="item.allowClear && item.allowClear == 1"
+                  :disabled="type == 1 ? item.addEditable != 1 : type == 2 ? item.updateEditable != 1 : false"
+                />
               </a-form-item>
             </a-col>
           </template>
@@ -97,17 +111,19 @@ import { auto_complete } from '@/api/autoComplete'
 export default {
   data() {
     return {
-      form: this.$form.createForm(this, { name: 'dynamic_crud_rule' }),
+      form: {},
       crudId: null,
       columns: [],
       type: null,
-      autoCompleteItem: {}
+      autoCompleteItem: {},
+      formKey: 'dynamic_crud_rule'
     }
   },
   mounted() {},
   methods: {
     async showAdd() {
       this.type = 1
+      this.form = this.$form.createForm(this, { name: this.formKey })
       await this.queryColumns()
       setTimeout(() => {
         this.autoFocus('addEditable')
@@ -116,14 +132,14 @@ export default {
     autoFocus(editable) {
       for (var i = 0; i < this.columns.length; i++) {
         if (this.columns[i][editable] != null && this.columns[i][editable] == 1) {
-          var id = 'dynamic_crud_rule_' + this.columns[i].name
+          var id = this.formKey + '_' + this.columns[i].name
           var _dom = document.getElementById(id)
           if (_dom.type == undefined) {
             var _classList = _dom.classList
             //日期组件
             if (_classList.contains('ant-calendar-picker')) {
               var input = _dom.getElementsByClassName('ant-calendar-picker-input')
-              _dom.focus()
+              input[0].focus()
             }
             //select组件
             if (_classList.contains('ant-select')) {
@@ -142,7 +158,6 @@ export default {
         this.columns = response.data.columns
         if (this.columns) {
           this.columns.forEach(s => {
-            //下拉
             if (s.relationTable != null && s.relationTable != '') {
               dynamic_crud_relation_item(s.id).then(response => {
                 s.relationItems = response.data
